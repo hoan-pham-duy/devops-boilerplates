69b79a9c5374592c4b0e1f37d6ee0deb97849e7f
Pham Duy Hoan <phamduyhoan95@gmail.com>
2023-07-06 23:23:55 +0000

    Add index.ts

--- backend/index.ts
+++ backend/index.ts
@@ -4,8 +4,15 @@ import express from 'express';
 // Initialize the express engine
 const app: express.Application = express();
 
+import dotenv from 'dotenv'
+import connectDB from './config/db'
+
+dotenv.config()
+
+connectDB()
+
 // Take a port 8000 for running server.
-const port: number = 8000;
+const port: number = parseInt(process.env.PORT !== undefined? process.env.PORT : "5000")
 
 // Handling '/' Request
 app.get('/', (_req, _res) => {
