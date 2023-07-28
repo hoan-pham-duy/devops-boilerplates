8355d9602bc1d028e1a473244dfce263bc7cf05f
Pham Duy Hoan <phamduyhoan95@gmail.com>
2023-07-28 05:17:13 +0000

    Update frontend

--- frontend/src/screen/HomeScreen.tsx
+++ frontend/src/screen/HomeScreen.tsx
@@ -1,12 +1,15 @@
-import React from 'react'
+import './HomeScreen.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faGithub} from '@fortawesome/free-brands-svg-icons'
 
 function HomeScreen() {
   const fetchRootBe: string = ''
-  
 
   return (
-    <div>HomeScreen</div>
-
+    <div className="home-screen">
+      <h3 className="title">DEVOPS BOILERPLATES</h3>
+      <button className="github"> <a>Github <FontAwesomeIcon icon={faGithub} /></a></button>
+    </div>
   )
 }
 
