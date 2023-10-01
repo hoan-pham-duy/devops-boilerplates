a4d084618c8fb2de265e128ca74add421c00a336
Pham Duy Hoan <phamduyhoan95@gmail.com>
2023-10-01 05:10:23 +0000

    Add gke-k8s-pilot/app.tf

---
+++ devops/terraform/google-cloud/gke-k8s-pilot/app.tf
@@ -0,0 +1,12 @@
+data "google_client_config" "default" {}
+
+provider "kubernetes" {
+  host                   = "https://${google_container_cluster.default.endpoint}"
+  token                  = data.google_client_config.default.access_token
+  cluster_ca_certificate = base64decode(google_container_cluster.default.master_auth[0].cluster_ca_certificate)
+
+  ignore_annotations = [
+    "^autopilot\\.gke\\.io\\/.*",
+    "^cloud\\.google\\.com\\/.*"
+  ]
+}
